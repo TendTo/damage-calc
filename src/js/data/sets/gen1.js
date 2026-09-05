3cace7df673b35a07b5db89a3ea4e12f390e743e
Automated <actions@users.noreply.github.com>
2026-09-05 01:06:18 +0000

    Update sets

--- src/js/data/sets/gen1.js
+++ src/js/data/sets/gen1.js
@@ -4,7 +4,7 @@ var SETDEX_RBY = {
     "NU Sleeper": {"moves":["Sleep Powder","Razor Leaf","Body Slam","Hyper Beam"]},
     "PU Sleeper": {"moves":["Sleep Powder","Razor Leaf","Body Slam","Hyper Beam"]},
     "1v1 SubSeed": {"moves":["Razor Leaf","Toxic","Leech Seed","Substitute"]},
-    "UU Showdown Usage": {"moves":["Sleep Powder","Razor Leaf","Body Slam","Swords Dance"]}
+    "UU Showdown Usage": {"moves":["Razor Leaf","Sleep Powder","Body Slam","Swords Dance"]}
   },
   "Charmander": {
     "LC Mixed Attacker": {"level":5,"moves":["Fire Blast","Body Slam","Counter","Substitute"]}
@@ -15,8 +15,7 @@ var SETDEX_RBY = {
   "Charizard": {
     "OU Swords Dance": {"moves":["Swords Dance","Hyper Beam","Earthquake","Slash"]},
     "NU Swords Dance": {"moves":["Swords Dance","Hyper Beam","Earthquake","Fire Blast"]},
-    "NU Mixed Attacker": {"moves":["Fire Blast","Slash","Earthquake","Body Slam"]},
-    "UU Showdown Usage": {"moves":["Earthquake","Fire Blast","Swords Dance","Hyper Beam"]}
+    "NU Mixed Attacker": {"moves":["Fire Blast","Slash","Earthquake","Body Slam"]}
   },
   "Squirtle": {
     "LC Special Attacker": {"level":5,"moves":["Surf","Blizzard","Body Slam","Counter"]}
@@ -451,6 +450,7 @@ var SETDEX_RBY = {
     "OU Swords Dance": {"moves":["Swords Dance","Hyper Beam","Slash","Submission"]},
     "PU Swords Dance": {"moves":["Swords Dance","Slash","Hyper Beam","Seismic Toss"]},
     "ZU Swords Dance": {"moves":["Swords Dance","Hyper Beam","Slash","Submission"]},
+    "UU Showdown Usage": {"moves":["Swords Dance","Hyper Beam","Seismic Toss","Body Slam"]},
     "Tradebacks OU Showdown Usage": {"moves":["Bind","Hyper Beam","Swords Dance","Slash"]}
   },
   "Tauros": {
@@ -482,8 +482,7 @@ var SETDEX_RBY = {
   "Vaporeon": {
     "NU Defensive": {"moves":["Surf","Body Slam","Blizzard","Rest"]},
     "NU Acid Armor": {"moves":["Surf","Acid Armor","Body Slam","Rest"]},
-    "Tradebacks OU Growth": {"moves":["Growth","Surf","Acid Armor","Rest"]},
-    "UU Showdown Usage": {"moves":["Rest","Surf","Acid Armor","Ice Beam"]}
+    "Tradebacks OU Growth": {"moves":["Growth","Surf","Acid Armor","Rest"]}
   },
   "Jolteon": {
     "OU Offensive": {"moves":["Thunder Wave","Thunderbolt","Double Kick","Rest"]},
@@ -517,8 +516,7 @@ var SETDEX_RBY = {
   },
   "Kabutops": {
     "OU Standard": {"moves":["Swords Dance","Slash","Hyper Beam","Surf"]},
-    "NU Bulky Attacker": {"moves":["Slash","Surf","Swords Dance","Hyper Beam"]},
-    "UU Showdown Usage": {"moves":["Hyper Beam","Swords Dance","Slash","Hydro Pump"]}
+    "NU Bulky Attacker": {"moves":["Slash","Surf","Swords Dance","Hyper Beam"]}
   },
   "Aerodactyl": {
     "Ubers Defensive Wall": {"moves":["Reflect","Rest","Double-Edge","Fire Blast"]},
